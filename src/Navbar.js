74f93981c3860bdf70858fb300cbd66b5b3a2b97
Vered Rekanati <veredrec@gmail.com>
2017-12-22 22:19:30 +0200

    adds people to page for future visualization Relates #8

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,13 +7,13 @@ class Navbar extends Component {
       <header className="header">
         <div className="logo-wrapper">
           <h1 className="main-title">The World of Refugees</h1>
-          {/* <img className="logo" src=""/> */}
         </div>
         <nav className="navbar">
           <ul className="menu">
-            <li className="menu-item">About</li>
+            <li className="menu-item">Refugees in the World</li>
             <li className="menu-item">Resources</li>
             <li className="menu-item">How Can I help?</li>
+            <li className="menu-item">About</li>
           </ul>
         </nav>
       </header>
