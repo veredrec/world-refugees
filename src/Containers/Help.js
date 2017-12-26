f0b18434be5fc86eb93f8779d33bc7a52beabcca
Vered Rekanati <veredrec@gmail.com>
2017-12-26 22:39:56 +0200

    render about, sources, and help pages Relates #18

--- src/Containers/Help.js
+++ src/Containers/Help.js
@@ -5,7 +5,29 @@ class Help extends Component {
   render() {
     return (
       <div className="help-section">
-        <h2 class="help-title"> This is HOW CAN I HELP page </h2>
+        <h2 className="help-title"> HOW CAN I HELP? </h2>
+        <ul className="help-menu">
+          <li className="help-link">
+            <a className="link" href="https://helprefugees.org/volunteer/">
+              Help Refugees
+            </a>
+          </li>
+          <li className="help-link">
+            <a className="link" href="http://www.greekislandvolunteers.com/">
+              Greek Islands Volunteers
+            </a>
+          </li>
+          <li className="help-link">
+            <a className="link" href="http://www.unhcr.org/get-involved.html">
+              UNHCR
+            </a>
+          </li>
+          <li className="help-link">
+            <a className="link" href="https://www.rescue.org/volunteer">
+              International Rescue Committee
+            </a>
+          </li>
+        </ul>
       </div>
     );
   }
--- src/Containers/Sources.js
+++ src/Containers/Sources.js
@@ -5,7 +5,24 @@ class Sources extends Component {
   render() {
     return (
       <div className="sources-section">
-        <h2 class="souces-title"> This is SOURCES page </h2>
+        <h2 className="sources-title"> For More Information </h2>
+        <ul className="sources-menu">
+          <li className="source-link">
+            <a className="source" href="https://en.wikipedia.org/wiki/Refugee">
+              Wikipedia - Refugees
+            </a>
+          </li>
+          <li className="help-link">
+            <a className="link" href="http://www.unhcr.org/en-ie/">
+              The UN Refugee Agency
+            </a>
+          </li>
+          <li className="help-link">
+            <a className="link" href="http://www.unhcr.org/get-involved.html">
+              The Refugee Project
+            </a>
+          </li>
+        </ul>
       </div>
     );
   }
